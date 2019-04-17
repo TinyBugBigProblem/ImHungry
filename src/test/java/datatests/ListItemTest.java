2c39a1e6a4b2b7a83b9a3b8154978befd8334a33
Jay Doshi <jaydoshi@usc.edu>
2019-04-16 19:50:54 -0700

    Update ListItemTest.java

--- src/test/java/datatests/ListItemTest.java
+++ src/test/java/datatests/ListItemTest.java
@@ -35,7 +35,9 @@ public class ListItemTest {
 		assertEquals(newRes, null);
 		
 		String type = item.getType();
-		assertTrue(type == "res");
+		//assertTrue(type == "res");
+		assertEquals(type,"res");
+
 		
 		
 	}
