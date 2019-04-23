eb7ebe372a8d7b0800534df1402a6ee2191245a1
Vincent Liang <liangvin@usc.edu>
2019-04-22 18:58:30 -0700

    String[]

--- src/main/resources/database/Database.java
+++ src/main/resources/database/Database.java
@@ -9,6 +9,10 @@ import java.sql.Statement;
 
 import org.json.simple.JSONObject;
 
+import data.Recipe;
+import data.Restaurant;
+import data.UserInfo;
+
 public class Database {
 	
 	private Connection conn = null;
@@ -16,13 +20,14 @@ public class Database {
 	private ResultSet rs = null;
 	private PreparedStatement ps = null;
 	
-	public Database(){
-	  connection();
+	public Database() {
+		
 	}
+	
 	private void connection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Info?user=root&password=root");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Info?user=root&password=root&useSSL=false");
 		} catch (SQLException sqle) {
 			System.out.println("SQLException: " + sqle.getMessage());
 		} catch (ClassNotFoundException cnfe) {
@@ -49,8 +54,9 @@ public class Database {
 	/* Handle User Information */
 	public String[] signUpUser(String username, String password) {
 		this.connection(); //connecting to database
-		String status = "false";
+		String status = "";
 		String comment = "";
+		String[] response = new String[2];
 
 		if (!this.findExistedUser(username)) {
 			try {
@@ -68,12 +74,11 @@ public class Database {
 				this.disconnectMySQL();
 			}
 			System.out.println("User: " + username + " SignUp Successfully.");
-			status =  "true";
+			status = "true";
 			comment = "User: " + username + " SignUp Successfully.";
-			String[] stringArray = new String[2];
-	    stringArray[0] = status;
-	    stringArray[1] = comment;
-			return stringArray;
+			response[0] = status;
+			response[1] = comment;
+			return response;
 		}
 		this.closeOperators();
 		this.disconnectMySQL();
@@ -81,16 +86,16 @@ public class Database {
 		System.out.println("Error: Username has been used.");
 		status =  "false";
 		comment = "Username: " + username + " has been used.";
-		String[] stringArray = new String[2];
-		stringArray[0] = status;
-		stringArray[1] = comment;
-		return stringArray;
+		response[0] = status;
+		response[1] = comment;
+		return response;
 	}
 	
-	public JSONObject signInUser(String username, String password) {
+	public String[] signInUser(String username, String password) {
 		this.connection(); //connecting to database
-		boolean status = false;
+		String status = "";
 		String comment = "";
+		String[] response = new String[2];
 		
 		try {
 			st = conn.createStatement();
@@ -103,14 +108,18 @@ public class Database {
 						+ "' AND UserPassword='" + hashedpasswrod + "';");
 				while (rs2.next()) {
 					System.out.println("User: " + username + " Login Successfully.");
-					status = true;
+					status = "true";
 					comment = "User: " + username + " Login Successfully.";
-					return getJsonObject(status, comment);
+					response[0] = status;
+					response[1] = comment;
+					return response;
 				}
 				System.out.println("Password is Incorrect.");
-				status = false;
+				status = "false";
 				comment = "Password is Incorrect.";
-				return getJsonObject(status, comment);
+				response[0] = status;
+				response[1] = comment;
+				return response;
 			}
 		} catch (SQLException sqle) {
 			System.out.println("Sign-In User Exception: " + sqle.getMessage());
@@ -119,19 +128,20 @@ public class Database {
 			this.disconnectMySQL();
 		}
 		System.out.println("Username does not exist.");
-		status =  false;
+		status =  "false";
 		comment = "Username: " + username + " does not exist.";
-		
-		return getJsonObject(status, comment);
+		response[0] = status;
+		response[1] = comment;
+		return response;
 	}
 	
-	private JSONObject getJsonObject(boolean status, String comment) {
-		JSONObject obj = new JSONObject();
-		obj.put("Status", status); // return boolean
-		obj.put("Comment", comment); // return comment
-		
-		return obj;
-	}
+//	private JSONObject getJsonObject(boolean status, String comment) {
+//		JSONObject obj = new JSONObject();
+//		obj.put("Status", status); // return boolean
+//		obj.put("Comment", comment); // return comment
+//		
+//		return obj;
+//	}
 	
 	/* Handle Grocery Information */
 	private boolean findExistedGroceryItem(String groceryname) { // check if groceryname exists
@@ -145,15 +155,16 @@ public class Database {
 					existed = true;
 			}
 		} catch (SQLException sqle) {
-			System.out.println("Sign-In User Exception: " + sqle.getMessage());
+			System.out.println("Find Existed Grocery Item Exception: " + sqle.getMessage());
 		} 
 		return existed;
 	}
 	
-	public JSONObject addGroceryItem(String groceryname, double amount, String units, String qualifier, double cost, String username) {
+	public String[] addGroceryItem(String groceryname, double amount, String units, String qualifier, double cost, String username) {
 		this.connection(); //connecting to database
-		boolean status = false;
+		String status = "";
 		String comment = "";
+		String[] response = new String[2];
 
 		if (!this.findExistedGroceryItem(groceryname)) {
 			try {
@@ -175,24 +186,28 @@ public class Database {
 				this.disconnectMySQL();
 			}
 			System.out.println("Grocery: " + groceryname + " is added.");
-			status =  true;
+			status =  "true";
 			comment = "Grocery: " + groceryname + " is added.";
-			return getJsonObject(status, comment);
+			response[0] = status;
+			response[1] = comment;
+			return response;
 		}
 		this.closeOperators();
 		this.disconnectMySQL();
 		
 		System.out.println("Error: Grocery is existed.");
-		status =  false;
+		status =  "false";
 		comment = "Grocery: " + groceryname + " is existed.";
-		
-		return getJsonObject(status, comment);
+		response[0] = status;
+		response[1] = comment;
+		return response;
 	}
 
-	public JSONObject removeGroceryItem(String groceryname, double amount, String units, String qualifier, double cost, String username) {
+	public String[] removeGroceryItem(String groceryname, double amount, String units, String qualifier, double cost, String username) {
 		this.connection(); //connecting to database
-		boolean status = false;
+		String status = "";
 		String comment = "";
+		String[] response = new String[2];
 
 		if (findExistedGroceryItem(groceryname)) {
 			try {
@@ -213,18 +228,251 @@ public class Database {
 				this.disconnectMySQL();
 			}
 			System.out.println("Remove GroceryItem: " + groceryname + " Successfully.");
-			status = true;
+			status = "true";
 			comment = "Remove GroceryItem: " + groceryname + " Successfully.";
-			return getJsonObject(status, comment);
+			response[0] = status;
+			response[1] = comment;
+			return response;
 		}
 		this.closeOperators();
 		this.disconnectMySQL();
 		
 		System.out.println("GroceryItem: " + groceryname + " does not exist.");
-		status = false;
+		status = "false";
 		comment = "GroceryItem: " + groceryname + " does not exist.";
-		return getJsonObject(status, comment);
+		response[0] = status;
+		response[1] = comment;
+		return response;
 	}
+	
+	/* Handle Favorite Restaurant */
+	private boolean findExistedFavoriteRestaurant(Restaurant restaurant) { // check if restaurant name exists
+		boolean existed = false;
+		try {
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM FavoriteRestaurant WHERE FRName='" + restaurant.getName() + "';");
+			while (rs.next()) {
+				String gname = rs.getString("FRName");
+				if (gname.equals(restaurant.getName()))
+					existed = true;
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Find Existed Favorite Restaurant Exception: " + sqle.getMessage());
+		} 
+		return existed;
+	}
+	
+	public String[] addFavoriteRestaurant(Restaurant restaurant, UserInfo user) {
+		this.connection(); //connecting to database
+		String status = "";
+		String comment = "";
+		String[] response = new String[2];
+
+		if (!this.findExistedFavoriteRestaurant(restaurant)) {
+			try {
+				ps = conn.prepareStatement(
+						"insert into FavoriteRestaurant(FRName, FRUrl, FRPrice, FRAddress, FRPhoneNumber, FRRating, FRDrivingTime, Username) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+
+				ps.setString(1, restaurant.getName());
+				ps.setString(2, restaurant.getWebsiteUrl());
+				ps.setInt(3, restaurant.getPrice());
+				ps.setString(4, restaurant.getAddress());
+				ps.setString(5, restaurant.getPhoneNumber());
+				ps.setDouble(6, restaurant.getRating());
+				ps.setInt(7, restaurant.getDrivingTime());
+				ps.setString(8, user.getUsername());
+				ps.executeUpdate();
+				
+			} catch (SQLException sqle) {
+				System.out.println("Add Favorite Restaurant Exception :" + sqle.toString());
+			} finally {
+				this.closeOperators();
+				this.disconnectMySQL();
+			}
+			System.out.println("Favorite Restaurant: " + restaurant.getName() + " is added.");
+			status =  "true";
+			comment = "Favorite Restaurant: " + restaurant.getName() + " is added.";
+			response[0] = status;
+			response[1] = comment;
+			return response;
+		}
+		this.closeOperators();
+		this.disconnectMySQL();
+		
+		System.out.println("Error: Favorite Restaurant is existed.");
+		status =  "false";
+		comment = "Favorite Restauran: " + restaurant.getName() + " is existed.";
+		response[0] = status;
+		response[1] = comment;
+		return response;
+	}
+
+	public String[] removeFavoriteRestaurant(Restaurant restaurant, UserInfo user) {
+		this.connection(); // connecting to database
+		String status = "";
+		String comment = "";
+		String[] response = new String[2];
+
+		if (findExistedFavoriteRestaurant(restaurant)) {
+			try {
+				String cmd = "DELETE FROM FavoriteRestaurant WHERE FRName = ? AND FRUrl = ? AND FRPrice = ? AND FRAddress = ? AND FRPhoneNumber = ? AND FRRating = ? AND FRDrivingTime = ? AND Username = ?";
+				ps = conn.prepareStatement(cmd);
+				ps.setString(1, restaurant.getName());
+				ps.setString(2, restaurant.getWebsiteUrl());
+				ps.setInt(3, restaurant.getPrice());
+				ps.setString(4, restaurant.getAddress());
+				ps.setString(5, restaurant.getPhoneNumber());
+				ps.setDouble(6, restaurant.getRating());
+				ps.setInt(7, restaurant.getDrivingTime());
+				ps.setString(8, user.getUsername());
+				ps.executeUpdate();
+				
+			} catch (SQLException sqle) {
+				System.out.println("Remove Favorite Restaurant Exception: " + sqle.getMessage());
+			} finally {
+				this.closeOperators();
+				this.disconnectMySQL();
+			}
+			System.out.println("Remove Favorite Restaurant: " + restaurant.getName() + " Successfully.");
+			status = "true";
+			comment = "Remove Favorite Restaurant: " + restaurant.getName() + " Successfully.";
+			response[0] = status;
+			response[1] = comment;
+			return response;
+		}
+		this.closeOperators();
+		this.disconnectMySQL();
+		
+		System.out.println("Favorite Restaurant: " + restaurant.getName() + " does not exist.");
+		status = "false";
+		comment = "Favorite Restaurant: " + restaurant.getName() + " does not exist.";
+		response[0] = status;
+		response[1] = comment;
+		return response;
+	}
+	
+	/* Handle Favorite Recipe */
+	private boolean findExistedFavoriteRecipe(Recipe recipe) { // check if restaurant name exists
+		boolean existed = false;
+		try {
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM FavoriteRecipe WHERE FPName='" + recipe.getName() + "';");
+			while (rs.next()) {
+				String gname = rs.getString("FPName");
+				if (gname.equals(recipe.getName()))
+					existed = true;
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Find Existed Favorite Recipe Exception: " + sqle.getMessage());
+		} 
+		return existed;
+	}
+	
+	public String[] addFavoriteRecipe(Recipe recipe, UserInfo user) {
+		this.connection(); //connecting to database
+		String status = "";
+		String comment = "";
+		String[] response = new String[2];
+
+		if (!this.findExistedFavoriteRecipe(recipe)) {
+			try {
+				ps = conn.prepareStatement(
+						"insert into FavoriteRecipe(FPName, FPUrl, FPPrepareTime, FPCookingTime, FPIngredients, FPInstructions, FPRating, Username) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+
+				ps.setString(1, recipe.getName());
+				ps.setString(2, recipe.getPictureUrl());
+				ps.setDouble(3, recipe.getPrepTime());
+				ps.setDouble(4, recipe.getCookTime());
+				String ingredients = "";
+				for(String ingredient: recipe.getIngredients()) {
+					ingredients += (ingredients == "" ? ingredient : ("," + ingredient));
+				}
+				ps.setString(5, ingredients);
+				String instructions = "";
+				for(String instruction: recipe.getInstructions()) {
+					instructions += (instructions == "" ? instruction : ("," + instruction));
+				}
+				ps.setString(6, instructions);
+				ps.setDouble(7, recipe.getRating());
+				ps.setString(8, user.getUsername());
+				ps.executeUpdate();
+				
+			} catch (SQLException sqle) {
+				System.out.println("Add Favorite Recipe Exception :" + sqle.toString());
+			} finally {
+				this.closeOperators();
+				this.disconnectMySQL();
+			}
+			System.out.println("Favorite Recipe: " + recipe.getName() + " is added.");
+			status =  "true";
+			comment = "Favorite Recipe: " + recipe.getName() + " is added.";
+			response[0] = status;
+			response[1] = comment;
+			return response;
+		}
+		this.closeOperators();
+		this.disconnectMySQL();
+		
+		System.out.println("Error: Favorite Recipe is existed.");
+		status =  "false";
+		comment = "Favorite Recipe: " + recipe.getName() + " is existed.";
+		response[0] = status;
+		response[1] = comment;
+		return response;
+	}
+
+	public String[] removeFavoriteRecipe(Recipe recipe, UserInfo user) {
+		this.connection(); // connecting to database
+		String status = "";
+		String comment = "";
+		String[] response = new String[2];
+
+		if (findExistedFavoriteRecipe(recipe)) {
+			try {
+				String cmd = "DELETE FROM FavoriteRecipe WHERE FPName = ? AND FPUrl = ? AND FPPrepareTime = ? AND FPCookingTime = ? AND FPIngredients = ? AND FPInstructions = ? AND FPRating = ? AND Username = ?";
+				ps = conn.prepareStatement(cmd);
+				
+				ps.setString(1, recipe.getName());
+				ps.setString(2, recipe.getPictureUrl());
+				ps.setDouble(3, recipe.getPrepTime());
+				ps.setDouble(4, recipe.getCookTime());
+				String ingredients = "";
+				for(String ingredient: recipe.getIngredients()) {
+					ingredients += (ingredients == "" ? ingredient : ("," + ingredient));
+				}
+				ps.setString(5, ingredients);
+				String instructions = "";
+				for(String instruction: recipe.getInstructions()) {
+					instructions += (instructions == "" ? instruction : ("," + instruction));
+				}
+				ps.setString(6, instructions);
+				ps.setDouble(7, recipe.getRating());
+				ps.setString(8, user.getUsername());
+				ps.executeUpdate();
+				
+			} catch (SQLException sqle) {
+				System.out.println("Remove Favorite Recipe Exception: " + sqle.getMessage());
+			} finally {
+				this.closeOperators();
+				this.disconnectMySQL();
+			}
+			System.out.println("Remove Favorite Recipe: " + recipe.getName() + " Successfully.");
+			status = "true";
+			comment = "Remove Favorite Recipe: " + recipe.getName() + " Successfully.";
+			response[0] = status;
+			response[1] = comment;
+			return response;
+		}
+		this.closeOperators();
+		this.disconnectMySQL();
+		
+		System.out.println("Favorite Recipe: " + recipe.getName() + " does not exist.");
+		status = "false";
+		comment = "Favorite Recipe: " + recipe.getName() + " does not exist.";
+		response[0] = status;
+		response[1] = comment;
+		return response;
+	}	
 	
 	private void closeOperators() {
 		try {
